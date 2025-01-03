09eaad37ebd8311d049d6bd3430eb011e7362201
Alok Singh <alokbeniwal@gmail.com>
2025-01-02 21:02:18 -0800

    wip

--- Atomization/Basic.lean
+++ Atomization/Basic.lean
@@ -1,12 +1,46 @@
-def hello := "world"
+import Lean.Meta
 
+def g := 1
+
+-- #eval Lean.Meta.inferType `f
 def f := 2
+def fg := g + g
+def f' : 2 = 2 := rfl
 
-namespace Foo
-def f := 2
+theorem f'' : 2 = 2 := by rfl
+
+def fib : Nat → Nat := fun n =>
+  match n with
+  | 0 => 0
+  | 1 => 1
+  | n + 2 => fib (n + 1) + fib n
+
+def fibImperative (n: Nat) : Nat := Id.run do
+  let mut a : Nat := 0
+  let mut b : Nat := 1
+  for i in [0:n] do
+    let c := a + b
+    a := b
+    b := c
+  return b
+-- max's idea of spec: there's 2 functions, one is the recursive one, the other is the imperative one. the recursive one is the 'spec' of the imperative one.
 
 
 
-macro "iff" term:term "thenn" term:term : term => `(term)
-
-end Foo
+def fib': Nat -> Nat := fun n =>
+  (let a := 0;
+      let b := 1;
+      do
+        let r ←
+          -- let col := [0:n];
+          forIn [0:n] ⟨a, b⟩ fun i r =>
+            let a := r.fst;
+            let b := r.snd;
+            let c := a + b;
+            let a := b;
+            let b := c;
+            do
+                pure PUnit.unit
+                pure (ForInStep.yield ⟨a, b⟩)
+        match r with
+          | ⟨a, b⟩ => pure b).run
--- Main.lean
+++ Main.lean
@@ -1,7 +1,7 @@
 import Atomization
 import ImportGraph
 import LeanExtras
-
+import Lean.Elab
 open Lean Elab Command
 -- TODO: a hack to get Repr for Lean.NameSet to print `#eval`s
 
@@ -9,11 +9,14 @@ open Lean Elab Command
 deriving instance Repr for Lean.NameSet
 deriving instance Repr for Std.HashMap
 
--- Make `Syntax` hashable for `matchOnTree` to put in `HashSet` later
 deriving instance Hashable for Substring
+
 deriving instance Hashable for SourceInfo
 deriving instance Hashable for Syntax.Preresolved
 deriving instance Hashable for Syntax
+/-- Make `Syntax` hashable for `matchOnTree` to put in `HashSet` later-/
+instance : Hashable Syntax where
+  hash := fun syn => Hashable.hash syn
 
 
 
@@ -31,10 +34,16 @@ def getInfoTreesForFile
 
 #print Int
 
+
+-- #eval Lean.LocalDecl.toExpr
+
 /--Extracts the syntax from an info tree-/
 def matchOnTree (tree : InfoTree) : Syntax :=
   match tree with
-  | .context _ tree => matchOnTree tree
+  |.context _partialContext tree => matchOnTree tree
+  -- | .context partialContext tree => match partialContext with
+  --   | .commandCtx info => info.fileMap.source
+  --   | .parentDeclCtx name => do matchOnTree (← name.getInfoTrees)
   | .node (.ofTacticInfo info) _
   | .node (.ofTermInfo info) _
   | .node (.ofCommandInfo info) _
@@ -52,19 +61,37 @@ def matchOnTree (tree : InfoTree) : Syntax :=
   -- |.node (.ofChoiceInfo info) children => info.stx
   | _ => Syntax.missing
 
+
+-- idea: get all files that deps are defined in, then atomize those files
+#eval Lean.Meta.inferType (Lean.Expr.lit (.natVal 1))
 elab "#atomize " path:str : command => do
-  let trees ← getInfoTreesForFile (System.FilePath.mk ( path.getString))
-  logInfo s!"{trees.size}"
+  let maybeInfo := (<-getEnv).find? `f'
+  if h:maybeInfo.isSome then
+    let info := maybeInfo.get h
+    let name := info.value!
+    let type <- liftTermElabM (Lean.Meta.inferType name)
+    -- logInfo s!"{name}"
+    logInfo s!"{type}"
+    -- else
+      -- throwError "Int not found"
+
+  let path := System.FilePath.mk path.getString
+  let trees ← getInfoTreesForFile path
+  -- logInfo s!"{trees.size}"
   for tree in trees do
     let syn := matchOnTree tree
     let cmd <- liftCoreM (Lean.PrettyPrinter.formatCommand syn)
     logInfo s!"{cmd}"
 
+#atomize "Atomization/Basic.lean"
 
 -- `Lean.Syntax.Traverser`
+deriving instance Lean.ToJson for Lean.Name
 
+def _root_.Lean.Name.toJson  (name: Lean.Name) : Lean.Json := Lean.ToJson.toJson name
+
+#eval (`fg).toJson
 #eval (`Int).transitivelyUsedConstants
-
 elab "#atomize_name " name:ident : command => do
   let name := name.getId
   let trees ← name.getInfoTrees
@@ -79,6 +106,11 @@ elab "#atomize_name " name:ident : command => do
 -- Chris Bailey: Constant name -> pretty printer output
 
 def main : IO Unit := do
+  let trees ← (`Int).getInfoTrees
+  for tree in trees do
+    let syn := matchOnTree tree
+    -- let cmd <- liftCoreM (Lean.PrettyPrinter.formatCommand syn)
+    -- logInfo s!"{cmd}"
   return ()
 
 #eval (`Int).transitivelyUsedConstants
