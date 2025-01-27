20dd544ae42109bc24299a859f3d1af3312ef1d0
Seth Ahrenbach <ahrenbach.seth@gmail.com>
2025-01-27 13:23:39 -0600

    add 192.168 to privileges

--- scripts/grant_privileges.sh
+++ scripts/grant_privileges.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-mysql -u root -p${DB_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${DB_DB}.* TO 'root'@'172.%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -p${DB_PASSWORD} -e "
+GRANT ALL PRIVILEGES ON ${DB_DB}.* TO 'root'@'172.%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_DB}.* TO 'root'@'192.168.%' IDENTIFIED BY '${DB_PASSWORD}';
+"
